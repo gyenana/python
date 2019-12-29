76c58332b207855989febe4276ae9be6ba850a30
kyu <gkaansrb@coupang.com>
2019-12-29 17:50:57 +0900

    lambda source 정리

--- 계산기 만들기 - 2.py
+++ 계산기 만들기 - 2.py
@@ -1,27 +1,27 @@
 # 계산기 함수 만들기
 def caculator():
     try:
-        a = int(input('숫자를 입력하세요:'))  # 첫번째 초기 값
+        result = int(input('숫자를 입력하세요:'))  # 첫번째 초기 값
         while True:
             c = str(input('연산자를 입력하세요:'))
             if c == '=':
-                print(a)
+                print(result)
                 break
             else:
                 if c == '+':
                     b = int(input('숫자를 입력하세요:'))
-                    result = (lambda inputA, inputB: inputA + inputB)(a, b)
+                    result = (lambda inputA, inputB: inputA + inputB)(result, b)
                     # 앞에서는 def 로 했던 부분을 람다로 대체
                     # 람다 함수의 값을 result 변수에 새롭게 할당
                 elif c == '-':
                     b = int(input('숫자를 입력하세요:'))
-                    result = (lambda inputA, inputB: inputA - inputB)(a, b)
+                    result = (lambda inputA, inputB: inputA - inputB)(result, b)
                 elif c == '*':
                     b = int(input('숫자를 입력하세요:'))
-                    result = (lambda inputA, inputB: inputA * inputB)(a, b)
+                    result = (lambda inputA, inputB: inputA * inputB)(result, b)
                 elif c == '/':
                     b = int(input('숫자를 입력하세요:'))
-                    result = (lambda inputA, inputB: inputA / inputB)(a, b)
+                    result = (lambda inputA, inputB: inputA / inputB)(result, b)
                 else:
                     print("올바른 연산자를 입력해야지요!")
                     break
