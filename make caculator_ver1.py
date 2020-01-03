3893cc4d4636b1e2c8773d97340464face37362b
gyenana <thdrpsk94@gmail.com>
2020-01-04 00:38:21 +0900

    계산기 만들기 2버전 - 1버전에서 함수로 정의했던 부분을 람다로 대체

--- make caculator_ver1.py
+++ make caculator_ver1.py
@@ -11,7 +11,8 @@ def divide(a,b):
 #계산기 함수 만들기
 def caculator():
     try:
-        result=int(input('숫자를 입력하세요:')) #첫번째 초기 값
+        # 첫번째 초기 값
+        result=int(input('숫자를 입력하세요:'))
         while True:
             c=str(input('연산자를 입력하세요:'))
             if c == '=':
@@ -30,15 +31,19 @@ def caculator():
                 elif c=='/':
                     b = int(input('숫자를 입력하세요:'))
                     result=divide(result,b)
-                else: #연산자 이상한거 넣었을 때
+                else:
+                    # 연산자 이상한거 넣었을 때
                     print("올바른 연산자를 입력해야지요!")
                     break
                 print(result)
-    except ValueError: #이상한값 넣었을 때, 숫자에 문자 넣었을 때
+    except ValueError:
+        # 이상한값 넣었을 때, 숫자에 문자 넣었을 때
         print("올바른 값을 넣어야지요!")
-    except ZeroDivisionError: #영으로 나누었을 때
+    except ZeroDivisionError:
+        # 영으로 나누었을 때
         print("0으로 나누면 안되지요 !")
-    except: #그 외의 모든 에러
+    except:
+        # 그 외의 모든 에러
         print("뭔진모르지만 뭔가 잘못한게 분명하다!")
 
 caculator()
---
+++ test.py
@@ -0,0 +1,12 @@
+numbers=[273,103,5,32,65,9,72,800,99]
+
+for number in numbers:
+    if number%2==0:
+        print(number,"는 짝수입니다.")
+    else:
+        print(number,"는 홀수입니다.")
+
+for number in numbers:
+    number_str=str(number )
+    len_number=len(number_str)
+    print(number,"는",len_number,"자릿수입니다.")
