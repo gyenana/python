d1179d2b461e105298ebd8e7373e13b677f31317
gyenana <thdrpsk94@gmail.com>
2020-01-04 14:59:54 +0900

    * 계산기만들기

--- github/input_reverse_quest/main_program.py
+++ github/input_reverse_quest/main_program.py
@@ -6,9 +6,9 @@ while True :
         result=''
         arg = input_str.input_str()
         b = str(input('입력한 문자열을 출력시킬 방향을 선택하세요./ F(정방향) or R(역방향) : '))
-        if b=='F':
+        if b=='F' or b=='f':
             result = print_input.forward_print(arg)
-        elif b=='R':
+        elif b=='R' or b=='r':
             result = reverse_input.reverse_print(arg)
         break
         print(result)
